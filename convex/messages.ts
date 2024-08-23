820f7c87835589c0d8462686a13b382414845248
niranjannnnnn <amenu4aie21148@am.students.amrita.edu>
2024-08-23 22:02:48 +0530

    get messages added

--- convex/messages.ts
+++ convex/messages.ts
@@ -41,3 +41,73 @@ export const sendTextMessage = mutation({
     });
   },
 });
+
+export const getMessages = query({
+	args: {
+		conversation: v.id("conversations"),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("Unauthorized");
+		}
+
+		const messages = await ctx.db
+			.query("messages")
+			.withIndex("by_conversation", (q) => q.eq("conversation", args.conversation))
+			.collect();
+
+		const userProfileCache = new Map();
+
+		const messagesWithSender = await Promise.all(
+			messages.map(async (message) => {
+				let sender;
+				// Check if sender profile is in cache
+				if (userProfileCache.has(message.sender)) {
+					sender = userProfileCache.get(message.sender);
+				} else {
+					// Fetch sender profile from the database
+					sender = await ctx.db
+						.query("users")
+						.filter((q) => q.eq(q.field("_id"), message.sender))
+						.first();
+					// Cache the sender profile
+					userProfileCache.set(message.sender, sender);
+				}
+
+				return { ...message, sender };
+			})
+		);
+
+		return messagesWithSender;
+	},
+});
+
+// Unoptimized fetching all times
+// export const getMessages = query({
+//   args: {
+//     conversation: v.id("conversations"),
+//   },
+//   handler: async (ctx, args) => {
+//     const identity = await ctx.auth.getUserIdentity();
+//     if (!identity) {
+//       throw new Error("Unauthorized");
+//     }
+//     const messages = await ctx.db
+//       .query("messages")
+//       .withIndex("by_conversation", (q) =>
+//         q.eq("conversation", args.conversation)
+//       )
+//       .collect();
+
+//     const messageWithsender = await Promise.all(
+//       messages.map(async (message) => {
+//         const sender = await ctx.db
+//           .query("users")
+//           .filter((q) => q.eq(q.field("_id"), message.sender))
+//           .first();
+//         return { ...message, sender };
+//       })
+//     );
+//   },
+// });
